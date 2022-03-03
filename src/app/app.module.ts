6619b7f6d50bd501ffdf223f824787aa7313b39c
Gilles Axel Gui <guigilles@gmail.com>
2022-03-03 19:35:21 +0100

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import {provideFirebaseApp, initializeApp} from '@angular/fire/app';
+import {environment} from '../environments/environment';
+import {getAuth, provideAuth} from '@angular/fire/auth';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    // Firebase authentication import.
+    provideAuth(() => getAuth())],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebaseConfig: {
+    apiKey: 'AIzaSyAMjaoOw_H4XRuZmnHm0DN-jpKc6Ma52eM',
+    authDomain: 'storynextdoor.firebaseapp.com',
+    projectId: 'storynextdoor',
+    storageBucket: 'storynextdoor.appspot.com',
+    messagingSenderId: '564900807094',
+    appId: '1:564900807094:web:565f8ee7a3069893a20117',
+    measurementId: 'G-1MBZ3V8B1S'
+  }
 };
 
 /*
